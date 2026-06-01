e8caa13cebb54ab664fa1648143c8f6a6e7bd2c2
muditkalra <muditkalra267@gmail.com>
2026-06-01 12:08:56 +0530

    Added: 2144. Minimum Cost of Buying Candies With Discount

---
+++ Greedy/2144.Minimum Cost of Buying Candies With Discount.ts
@@ -0,0 +1,10 @@
+function minimumCost(cost: number[]): number {
+    cost.sort((a, b) => b - a);
+    let minCost = 0;
+    for (let i = 0; i < cost.length; i++) {
+        if (i % 3 !== 2) {
+            minCost += cost[i];
+        }
+    }
+    return minCost
+};
