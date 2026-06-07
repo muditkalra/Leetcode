8503e2b49331b188af076b87daa0a5e87599634b
muditkalra <muditkalra267@gmail.com>
2026-06-07 23:55:26 +0530

    added: 4sum

---
+++ Two-pointer/18.4Sum.ts
@@ -0,0 +1,35 @@
+function fourSum(nums: number[], target: number): number[][] {
+    let n = nums.length;
+    let res = [];
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < n; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        for (let j = i + 1; j < n; j++) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+            let k = j + 1;
+            let l = n - 1;
+
+            while (k < l) {
+                let sum = nums[i] + nums[j] + nums[k] + nums[l];
+
+                if (sum < target) {
+                    k++;
+                } else if (sum > target) {
+                    l--;
+                } else {
+                    res.push([nums[i], nums[j], nums[k], nums[l]]);
+                    k++;
+                    l--;
+                    while (k < l && nums[k] == nums[k - 1]) k++;
+                    while (k < l && nums[l] == nums[l + 1]) l--;
+                }
+            }
+        }
+    }
+
+    return res
+};
