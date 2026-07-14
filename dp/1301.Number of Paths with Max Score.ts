f58609399f9ef34846962d68f96ad8d8014e67d8
muditkalra <muditkalra267@gmail.com>
2026-07-14 13:54:40 +0530

    modified: 1301. Number of Paths with Max Score

--- dp/1301.Number of Paths with Max Score.ts
+++ dp/1301.Number of Paths with Max Score.ts
@@ -1,39 +1,76 @@
 function pathsWithMaxScore(board: string[]): number[] {
-    const MOD = 1000000007;
-    const n = board.length;
-    const dp: number[][][] = Array.from({ length: n }, () =>
-        Array.from({ length: n }, () => [-1, 0]),
-    );
-    dp[n - 1][n - 1][0] = 0;
-    dp[n - 1][n - 1][1] = 1;
+    let mod = 10 ** 9 + 7;
+    let n = board.length;
+    let dir = [[0, -1], [-1, 0], [-1, -1]];
+    let dp = Array.from({ length: n }, () => Array.from({ length: n }, () => [0, 0]));
+    dp[0][0] = [0, 1];
 
-    const update = (x: number, y: number, u: number, v: number): void => {
-        if (u >= n || v >= n || dp[u][v][0] === -1) {
-            return;
-        }
-        if (dp[u][v][0] > dp[x][y][0]) {
-            dp[x][y][0] = dp[u][v][0];
-            dp[x][y][1] = dp[u][v][1];
-        } else if (dp[u][v][0] === dp[x][y][0]) {
-            dp[x][y][1] = (dp[x][y][1] + dp[u][v][1]) % MOD;
-        }
-    };
+    function isValid(i: number, j: number) {
+        return i < n && j < n && i >= 0 && j >= 0;
+    }
 
-    for (let i = n - 1; i >= 0; i--) {
-        for (let j = n - 1; j >= 0; j--) {
-            if (!(i === n - 1 && j === n - 1) && board[i][j] !== "X") {
-                update(i, j, i + 1, j);
-                update(i, j, i, j + 1);
-                update(i, j, i + 1, j + 1);
-                if (dp[i][j][0] !== -1) {
-                    dp[i][j][0] +=
-                        board[i][j] === "E" ? 0 : parseInt(board[i][j]);
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] == "E") continue;
+            if (board[i][j] == "X") continue;
+
+            let max = 0;
+            let totalPath = 0;
+            for (let [dx, dy] of dir) {
+                let newX = i + dx;
+                let newY = j + dy;
+                if (isValid(newX, newY)) {
+                    let [score, path] = dp[newX][newY];
+                    if (score == max) {
+                        totalPath = (totalPath + path) % mod;
+                    } else if (score > max) {
+                        totalPath = path % mod;
+                        max = score;
+                    }
                 }
             }
+            max += board[i][j] !== "S" ? Number(board[i][j]) : 0;
+            max = totalPath == 0 ? 0 : max;
+            dp[i][j] = [max, totalPath];
         }
     }
-    if (dp[0][0][0] !== -1) {
-        return [dp[0][0][0], dp[0][0][1] % MOD];
-    }
-    return [0, 0];
-}
+
+
+
+    // function solve(i: number, j: number): number[] {
+    //     if (i == 0 && j == 0) {
+    //         return [0, 1];
+    //     }
+
+    //     if (board[i][j] == "X") {
+    //         return [0, 0];
+    //     }
+
+    //     if (dp[i][j]) {
+    //         return dp[i][j];
+    //     }
+
+    //     let max = 0;
+    //     let totalPath = 0;
+    //     for (let [dx, dy] of dir) {
+    //         let newX = i + dx;
+    //         let newY = j + dy;
+    //         if (isValid(newX, newY)) {
+    //             let [score, path] = solve(newX, newY);
+    //             if (score == max) {
+    //                 totalPath = (totalPath + path) % mod;
+    //             } else if (score > max) {
+    //                 totalPath = path % mod;
+    //                 max = score;
+    //             }
+    //         }
+    //     }
+
+    //     max += board[i][j] !== "S" ? Number(board[i][j]) : 0;
+    //     max = totalPath == 0 ? 0 : max;
+    //     dp[i][j] = [max, totalPath];
+    //     return [max, totalPath];
+    // }
+
+    return dp[n - 1][n - 1];
+};
