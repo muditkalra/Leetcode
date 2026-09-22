f98f88aa95c30edd5b5aa3d49218d909ab9126c3
muditkalra <muditkalra267@gmail.com>
2026-09-22 18:29:53 +0530

    added: 3524. Find X Value of Array I

---
+++ Dynamic-Programming/3524.Find X Value of Array I.ts
@@ -0,0 +1,24 @@
+function resultArray(nums: number[], k: number): number[] {
+    let n = nums.length;
+
+    let prevCount = new Array(k).fill(0);
+    let result = new Array(k).fill(0);
+
+    for (let i = 0; i < n; i++) {
+        let curCount = new Array(k).fill(0);
+
+        let currentElementRemainder = nums[i] % k;
+        curCount[currentElementRemainder] += 1;
+
+        for (let prevRem = 0; prevRem < k; prevRem++) {
+            let newRemain = (prevRem * nums[i] % k) % k;
+            curCount[newRemain] += prevCount[prevRem];
+        }
+        prevCount = curCount;
+
+        for (let x = 0; x < k; x++) {
+            result[x] += prevCount[x];
+        }
+    }
+    return result
+};
